a1f2fc76603e32120327ebf949ded5a2d36b4db5
Ujjwal Ojha <bring2uo@gmail.com>
2013-12-29 13:52:12 +0545

    Created Message Form

--- src/HtMessaging/Form/MessageForm.php
+++ src/HtMessaging/Form/MessageForm.php
@@ -2,7 +2,56 @@
     
 namespace HtMessaging\Form;
 
-class MessageForm
+use HtMessaging\Options\MultipleReceiversOptionsInterface;
+use ZfcBase\Form\ProvidesEventsForm;
+use Zend\Form\Element;
+
+class MessageForm extends ProvidesEventsForm
 {
-    
+    protected $options;
+
+    public function __construct(MultipleReceiversOptionsInterface $options, $name = 'message')
+    {
+        $this->options = $options;
+        parent::__construct($name);
+    }
+
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    public function init()
+    {
+        $this->add(array(
+            'name' => 'subject',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Subject',
+            ),
+            'attributes' => array(
+                'class' => 'form-control'
+            )
+        ));
+
+        $this->add(array(
+            'name' => 'message_text',
+            'type' => 'Textarea',
+            'options' => array(
+                'label' => 'Message Body',
+            ),
+            'attributes' => array(
+                'class' => 'form-control'
+            )
+        ));
+
+        $receiver = new Element\Select('receiver');
+        $receiver->setLabel('Receiver');
+        $receiver->setAttributes(array(
+            'class' => 'form-control',
+            'multiple' => $this->getOptions()->getEnableMultipleReceivers()
+        ));
+        $this->add($receiver);
+        $this->getEventManager()->trigger('init', $this);
+    }
 }
