3eb0fd2e832f1d5d40f50666135377fc7af23039
Stijn Haulotte <stijnhau@gmail.com>
2014-05-07 21:51:01 +0200

    Fixed mysql queries file.

--- data/mysql.sql
+++ data/mysql.sql
@@ -1,4 +1,13 @@
-﻿delimiter $$
+CREATE TABLE `message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `sender_id` int(11) NOT NULL,
+  `subject` varchar(64) NOT NULL,
+  `message_text` blob NOT NULL,
+  `created_date_time` datetime NOT NULL,
+  `starred_or_not` enum('1','0') NOT NULL DEFAULT '0',
+  `important_or_not` enum('1','0') NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `message_receiver` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -12,18 +21,4 @@ CREATE TABLE `message_receiver` (
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `message_receivers_fk1_idx` (`message_id`),
   CONSTRAINT `message_receivers_fk1` FOREIGN KEY (`message_id`) REFERENCES `message` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8$$
-
-
-delimiter $$
-
-CREATE TABLE `message` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `sender_id` int(11) NOT NULL,
-  `subject` varchar(64) NOT NULL,
-  `message_text` blob NOT NULL,
-  `created_date_time` datetime NOT NULL,
-  `starred_or_not` enum('1','0') NOT NULL DEFAULT '0',
-  `important_or_not` enum('1','0') NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8$$
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
